5f07a28d5b95d2db6e7b46b407d4ee741aab1166
Ryan Balfanz <rbalfanz@gmail.com>
2017-09-25 22:39:21 -0700

    Update AngularJS (v1.5.7) to Angular 4.2.4
    https://github.com/RyanBalfanz/ryanbalfanz.github.com/commit/50a2ea9a37196c98ea6668454a2c6a74095d580c

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = "I'm Ryan Balfanz";
+  links: {name: string, url: string, network: string}[] = [{
+		name: "Facebook",
+		url: "https://www.facebook.com/ryanbalfanz",
+		network: "facebook",
+	}, {
+		name: "Twitter",
+		url: "https://twitter.com/ryanbalfanz",
+		network: "twitter"
+	}, {
+		name: "LinkedIn",
+		url: "https://www.linkedin.com/in/ryanbalfanz",
+		network: "linkedin"
+	}, {
+		name: "GitHub",
+		url: "https://github.com/RyanBalfanz",
+		network: "github"
+	}];
 }
