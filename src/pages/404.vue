49d16b55f9dc929234c758b8b3cc906eb7dcc149
Ryan Balfanz <rbalfanz@gmail.com>
2019-04-24 14:23:32 -0700

    Add 404 page

---
+++ src/pages/404.vue
@@ -0,0 +1,23 @@
+<template>
+  <Layout>
+    <h1>Not found</h1>
+    <p>It's not here.</p>
+    <p>Back to <g-link to="/">{{ $static.metaData.siteName }}</g-link></p>
+  </Layout>
+</template>
+
+<static-query>
+query {
+  metaData {
+    siteName
+  }
+}
+</static-query>
+
+<script>
+export default {
+  metaInfo: {
+    title: 'Not Found'
+  }
+}
+</script>
